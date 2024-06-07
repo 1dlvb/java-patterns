4bbe98972f625b5b6201681be30373d4d761f023
1dlvb <matushkin-2017@mail.ru>
2024-06-07 15:59:17 +0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Java-patterns
+
+This is a repository for educational purposes. Testing how to work with patterns in Java
---
+++ src/main/java/ru/patterns/App.java
@@ -0,0 +1,15 @@
+package ru.patterns;
+
+/**
+ * Hello world!
+ *
+ */
+public final class App {
+
+    private App() {}
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
