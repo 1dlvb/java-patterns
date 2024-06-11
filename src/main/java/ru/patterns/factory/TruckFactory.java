e4f224a2d433e046c41f5c7961dba8796f7958b7
1dlvb <matushkin-2017@mail.ru>
2024-06-11 17:29:48 +0500

    docs: add @author tag to the doc

--- src/main/java/ru/patterns/factory/TruckFactory.java
+++ src/main/java/ru/patterns/factory/TruckFactory.java
@@ -2,6 +2,7 @@ package ru.patterns.factory;
 
 /**
  * Factory class, that should be used to create instances of Trucks
+ * @author Matushkin Anton
  */
 public class TruckFactory implements LogisticsFactory<RollingTransport> {
 
